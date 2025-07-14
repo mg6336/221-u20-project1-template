5e07bc0b23eb2db99ef747ccef42068b2f35fa6f
Matthew Gessner <mg6336@students.necsd.net>
2025-07-14 14:08:01 -0400

    New content

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -22,16 +22,18 @@ function gotolocation(path) {
 
 
 window.addEventListener("load" , function() {
-  this.document.getElementById("content").innerHTML = "hello worlds";
-  var article1 = {title : "Article" ,body : "Marist is very cool!" ,linkURL : "https://www.marist.edu/" ,imageURL : "images/campus.png"};
-  var article2 = {title : "Article of the topic" ,body : "This article is towards Newbrugh schools in which we talk about how Marist has helped the students in coding classes" ,linkURL : "https://www.newburghschools.org/" ,imageURL : "images/news_pic.jpg"};
-  var article3 = {title : "Bird Bird Bird" ,body : "BIRD IS THE WORD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" ,linkURL : "https://en.wikipedia.org/wiki/Bird" ,imageURL : "images/hancock.jpeg"};
-  var articles = [article1 , article2 , article3];
+  this.document.getElementById("content").innerHTML = "";
+  let content = document.getElementById("content");
+  let article1 = {title : "Article" , body: "Marist is very cool!" , linkURL : "https://www.marist.edu/" , imageURL : "images/campus.png"};
+  let article2 = {title : "Article of the topic" , body: "This article is towards Newbrugh schools in which we talk about how Marist has helped the students in coding classes" , linkURL : "https://www.newburghschools.org/" , imageURL : "images/news_pic.jpg"};
+  let article3 = {title : "Bird Bird Bird" , body: "All about birds" , linkURL : "https://en.wikipedia.org/wiki/Bird" , imageURL : "images/hancock.jpeg"};
+  let articles = [article1 , article2 , article3];
   for(i = 0; i < articles.length; i++){
-    content.innerHTML += "<h3>" articles[i].title  "</h3>";
-    content.innerHTML += "<h5>" articles[i].body  "</h5>";
-    content.innerHTML += "<h5>" articles[i].linkURL  "</h5>";
-    content.innerHTML += "<h5>" articles[i].imageURL  "</h5>";
+    console.log(articles[i]);
+    content.innerHTML += "<h3>" + articles[i].title + "</h3>";
+    content.innerHTML += "<h5>" + articles[i].body + "</h5>";
+    content.innerHTML += "<h5>" + articles[i].linkURL + "</h5>";
+    content.innerHTML += "<h5>" + articles[i].imageURL +  "</h5>";
   }
   
 })
